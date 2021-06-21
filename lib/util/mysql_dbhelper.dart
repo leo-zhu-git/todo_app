426894f8db6de4b00056fb7ae24dd85d41bf8060
leo-zhu-git <76552894+leo-zhu-git@users.noreply.github.com>
2021-06-21 23:34:53 +0800

    commit to master

--- lib/util/mysql_dbhelper.dart
+++ lib/util/mysql_dbhelper.dart
@@ -47,7 +47,7 @@ class MySql_DBHelper {
         String dbUserID = swiperDataList[i]['TaskUserId'].toString();
         String appTaskID = dbTaskID.substring(dbUserID.length, dbTaskID.length);
         print(appTaskID);
-        print("woaibeijingtiananmen,jalsdjflksdjf");
+        print("woaibeijingtiananmen,jalsdjflksdjf+1");
         Task task = Task.withId(
             int.parse(appTaskID),
             swiperDataList[i]['TaskTitle'],
