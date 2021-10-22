dcbbb6fa585547a7efdea151360dccb92012fb4c
KKMega <88682804+KKMega@users.noreply.github.com>
2021-10-22 21:57:01 +0530

    Update tasksearch.dart

--- lib/screens/tasksearch.dart
+++ lib/screens/tasksearch.dart
@@ -769,12 +769,10 @@ class TaskSearchState extends State {
                               DateFormat('yyyy-MM-dd').format(now);
                           if (value == true) {
                             this.tasklist[position].isDone = 1;
-                            this.tasklist[position].status = "Completed";
                             this.tasklist[position].dateDone = formattedDate;
                             dbHelper.updateTask(tasklist[position]);
                           } else {
                             this.tasklist[position].isDone = 0;
-                            this.tasklist[position].status = "Open";
                             this.tasklist[position].dateDone = '';
                             dbHelper.updateTask(tasklist[position]);
                           }
@@ -978,7 +976,7 @@ class TaskSearchState extends State {
   }
 
   void navigateToDetail(Task task) async {
-  await Navigator.push(
+    await Navigator.push(
       context,
       MaterialPageRoute(builder: (context) => TaskDetail(task)),
     );
