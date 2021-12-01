ed7f05bf5deeab10f18cc234768402824e07bf00
freeborders88 <rkltan@gmail.com>
2021-12-01 18:17:24 +0800

    piggy

--- lib/screens/taskdetail.dart
+++ lib/screens/taskdetail.dart
@@ -491,7 +491,8 @@ class TaskDetailState extends State //<TaskDetail>
                 });
               }),
           IconButton(
-              icon: Icon(Icons.save_alt, color: Colors.white),
+//              icon: Icon(Icons.save_alt, color: Colors.white),
+              icon: Icon(Icons.savings_outlined , color: Colors.white),
               tooltip: 'Save',
               onPressed: () {
                 setState(() {
