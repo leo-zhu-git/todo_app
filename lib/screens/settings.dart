bcaaffb3a928567b5771f21a3caf8bc2eacd04b1
KKMega <88682804+KKMega@users.noreply.github.com>
2022-01-08 15:37:03 +0530

    Update settings.dart

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -2,30 +2,25 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 
-
 class SettingsForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() => SettingsFormState();
 }
 
 class SettingsFormState extends State {
-  
-    @override
-    Widget build(BuildContext context) {
-      TextStyle? textStyle = Theme.of(context).textTheme.headline6;
-      return Scaffold(
-        backgroundColor: Colors.yellow[200],
-        appBar: AppBar(
-          backgroundColor: Colors.teal[800],
-          automaticallyImplyLeading: false,
-          title: Center(
-            child: Text('Capture It!'),
-          ),
+  @override
+  Widget build(BuildContext context) {
+    TextStyle? textStyle = Theme.of(context).textTheme.headline6;
+    return Scaffold(
+      backgroundColor: Colors.yellow[200],
+      appBar: AppBar(
+        backgroundColor: Colors.teal[800],
+        automaticallyImplyLeading: false,
+        title: Center(
+          child: Text('Capture It!'),
         ),
-        body: Text('hi'),
-      );
-    }
-
+      ),
+      body: Text('hi'),
+    );
+  }
 }
-
-
