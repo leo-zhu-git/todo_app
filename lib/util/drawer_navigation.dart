fdff2127fe884c0f9e7ba97a43f4bdd627f5a6d9
freeborders88 <rkltan@gmail.com>
2022-02-13 14:33:19 +0800

    typo

--- lib/util/drawer_navigation.dart
+++ lib/util/drawer_navigation.dart
@@ -138,7 +138,7 @@ class _DrawerNagivation extends State<DrawerNagivation> {
                     context: context,
                     applicationName: 'todoMIT',
                     applicationVersion: '1.0.1',
-                    applicationLegalese: 'todoMIT will help you manager your Most Important Things / Tasks / Thoughts on [almost] any mobile device! ',
+                    applicationLegalese: 'todoMIT will help you manage your Most Important Things / Tasks / Thoughts on [almost] any mobile device! ',
                   );
                 },
 
